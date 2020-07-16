e817eb1b6261731eb2aeddaf461725281a77d14d
tanyagupta7 <54990765+tanyagupta7@users.noreply.github.com>
2020-07-16 19:53:37 +0530

    Update and rename optimisedbubbleesort.java to optimisedbubblesort.java

--- optimisedbubblesort.java
+++ optimisedbubblesort.java
@@ -1,7 +1,7 @@
 package data;
 import java.util.*;
 
-public class optimisedbubbleesort {
+public class optimisedbubblesort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
