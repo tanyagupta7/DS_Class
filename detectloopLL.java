4e95b7fd37c4e282f48f58e80345bafb2c1e7bce
tanyagupta7 <54990765+tanyagupta7@users.noreply.github.com>
2020-08-07 23:44:47 +0530

    Add files via upload

--- detectloopLL.java
+++ detectloopLL.java
@@ -1,6 +1,20 @@
 package data;
 
 import java.util.Scanner;
+class Node {
+
+	
+    int data;
+    Node next;
+    
+    Node(int data)
+    {
+ 	   this.data=data;
+ 	   next=null;
+    }
+	
+
+}
 
 public class detectloopLL {
 	
